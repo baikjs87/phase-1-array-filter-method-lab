30d73e8d8bb4623e6dc854febbf2b7ba6df635f6
Jason Baik <baikjs87@gmail.com>
2022-03-31 22:53:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+
+function findMatching(array, string) {
+    const newString = string.toLowerCase();
+    const driverList = array.filter(function(element) {
+        const newElement = element.toLowerCase();
+        if(newElement === newString) {
+            return element;
+        }
+    })
+    return driverList;
+}
+
+function fuzzyMatch(array, string) {
+    const driverList = array.filter(driverName => driverName.startsWith(string));
+    return driverList;
+}
+
+function matchName(array, string) {
+    const driverList = array.filter(function(driver) {
+        const driverName = driver.name;
+        if(string === driverName) {
+            return driver;
+        }
+    })
+    return driverList;
+}
